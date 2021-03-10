48120314c48b113203d9ffdbd95cce7b4f3d7c41
Seongwon97 <osw970325@naver.com>
2021-03-10 22:36:31 +0900

    Example_BottomSheet_1

---
+++ Example_BottomSheet/app/src/main/java/gachon/example/example_bottomsheet/MainActivity.java
@@ -0,0 +1,32 @@
+package gachon.example.example_bottomsheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomSheetBehavior bottomSheetBehavior;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout linearLayout = findViewById(R.id.schedule_bottom_sheet);
+
+        bottomSheetBehavior = BottomSheetBehavior.from(linearLayout);
+
+        Button button = findViewById(R.id.btn_show_scheduler_bottom_sheet);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        });
+    }
+}
