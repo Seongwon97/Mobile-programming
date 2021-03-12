f7d77db7980cc89c540a065260640ca31872a831
Seongwon97 <osw970325@naver.com>
2021-03-12 23:19:20 +0900

    Example_customview

---
+++ Example_customview/app/src/main/java/gachon/example/example_customview/MyDrawEx.java
@@ -0,0 +1,30 @@
+package gachon.example.example_customview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.jar.Attributes;
+
+public class MyDrawEx extends View {
+    public MyDrawEx(Context c){
+        super(c);
+    }
+    public MyDrawEx(Context c, AttributeSet a){
+        super(c,a);
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color. RED);
+
+        canvas.drawColor(Color. WHITE);
+        for (int x=0; x<200; x+=5) {
+            canvas.drawLine(x, 0, 200-x, 100, paint);
+        }
+    }
+}
