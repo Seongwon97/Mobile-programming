69d50811e94ef24b7d24b1c40d926e748b2b5389
Seongwon97 <osw970325@naver.com>
2021-03-18 18:25:08 +0900

    Example_Drawable_java

---
+++ Example_Drawable_java/app/src/main/java/gachon/example/example_drawable_java/MainActivity.java
@@ -0,0 +1,29 @@
+package gachon.example.example_drawable_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout mLinearLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*mLinearLayout=new LinearLayout(this);
+
+        ImageView img=new ImageView(this);
+        Drawable myImage =getResources().getDrawable(R.drawable.ic_launcher);
+        img.setImageDrawable(myImage);
+        img.setLayoutParams(new Gallery.LayoutParams(Gallery.LayoutParams.WRAP_CONTENT,Gallery.LayoutParams.WRAP_CONTENT));
+
+        mLinearLayout.addView(img);
+
+        setContentView(mLinearLayout);
+    */}
+}
